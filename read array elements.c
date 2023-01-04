792dbfbc20e3fe97f8e66d66ce7fd866d2a300db
vijayatharani99 <116818442+vijayatharani99@users.noreply.github.com>
2023-01-04 23:11:34 +0530

    Add files via upload

---
+++ read array elements.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int arr[10];
+    int *pa;
+    int  i;
+    pa=&arr[0];
+    printf("Enter array elements:\n");
+    for(i=0;i < 10; i++){
+        printf("Enter element %02d: ",i+1);
+        scanf("%d",pa+i);   //reading through pointer
+    }
+
+    printf("\nEntered array elements are:");
+    printf("\nAddress\t\tValue\n");
+    for(i=0;i<10;i++){
+        printf("%08X\t%03d\n",(pa+i),*(pa+i));
+    }
+     return 0;
+}
